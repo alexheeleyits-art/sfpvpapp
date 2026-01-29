6542067bc374fcb8e4ce4ed555432054a90863c1
Alexh33 <heeleyalex@gmail.com>
2026-01-29 21:55:30 +0000

    Add CORS headers for battle state

--- api/battle-state.js
+++ api/battle-state.js
@@ -1,6 +1,15 @@
 const { kv } = require('@vercel/kv');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
   if (req.method !== 'GET') {
     res.status(405).send('Method Not Allowed');
     return;
